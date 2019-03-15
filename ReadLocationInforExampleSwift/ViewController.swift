27519adf226026a64037c8fb00872b45b9d7d3c8
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-14 21:17:42 -0400

    Location code added

--- ReadLocationInforExampleSwift/ViewController.swift
+++ ReadLocationInforExampleSwift/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, CLLocationManagerDelegate
+{
+    @IBOutlet weak var lblLng: UILabel!
+    @IBOutlet weak var lblLat: UILabel!
+    
+    var locationManager: CLLocationManager! = nil
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            
+            print(location.coordinate)
+            self.lblLat.text = "Latitude  : \(location.coordinate.latitude)"
+            self.lblLng.text = "Longitude : \(location.coordinate.longitude)"
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            showLocationDisabledPopUp()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error :\(error)")
+    }
+    
+    // Show the popup to the user if we have been deined access
+    func showLocationDisabledPopUp() {
+        let alertController = UIAlertController(title: "Background Location Access Disabled",
+                                                message: "In order to deliver pizza we need your location",
+                                                preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
